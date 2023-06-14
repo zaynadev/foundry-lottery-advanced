83f91d77d35b4cfd1e34ea05510ad5b5738dddcc
zaynadev <zayna.oufkir@gmail.com>
2023-06-14 21:33:47 +0100

    add pickWinner function with VRF chainlink

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,6 +2,10 @@
 
 pragma solidity 0.8.19;
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
+
 /**
  * @author  Zainab OUFQIR
  * @title   A simple raffle contract
@@ -9,30 +13,124 @@ pragma solidity 0.8.19;
  * @notice  This contract is for creating a simple raffle
  */
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /** Errors */
     error Raffle__NotEnoughEthSent();
+    error Raffle__TransferFailed();
+    error Raffle__RaffleNotOpen();
+
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
+    uint16 private constant REQUEST_CONFIRMATION = 3;
+    uint32 private constant NUM_WORDS = 1;
 
     uint private immutable i_entranceFee;
+    /** @dev Duration of the lottery in seconds  */
+    uint private immutable i_interval;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLine;
+    uint64 private immutable i_subscriptionID;
+    uint32 private immutable i_callbackGasLimit;
     address payable[] private s_players;
+    uint private s_lastTimeStamp;
+    address private s_recentWinner;
+    RaffleState private s_raffleState;
 
     /** Events */
     event EnteredRaffle(address indexed player);
+    event WinnerPicked(address indexed winner);
 
-    constructor(uint _entranceFee) {
+    constructor(
+        uint _entranceFee,
+        uint _interval,
+        address _vrfCoordinator,
+        uint64 _subscriptionID,
+        bytes32 _gasLine,
+        uint32 _callbackGasLimit
+    ) VRFConsumerBaseV2(_vrfCoordinator) {
         i_entranceFee = _entranceFee;
+        i_interval = _interval;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(_vrfCoordinator);
+        i_gasLine = _gasLine;
+        i_subscriptionID = _subscriptionID;
+        i_callbackGasLimit = _callbackGasLimit;
+        s_lastTimeStamp = block.timestamp;
+        s_raffleState = RaffleState.OPEN;
     }
 
     function enterRaffle() external payable {
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughEthSent();
         }
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__RaffleNotOpen();
+        }
         s_players.push(payable(msg.sender));
 
         emit EnteredRaffle(msg.sender);
     }
 
-    function getEntranceFee() external view returns (uint) {
+    function pickWinner() external {
+        if (block.timestamp - s_lastTimeStamp < i_interval) {
+            revert();
+        }
+        s_raffleState = RaffleState.CALCULATING;
+        // Will revert if subscription is not set and funded.
+        uint requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLine,
+            i_subscriptionID,
+            REQUEST_CONFIRMATION,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {
+        uint indexOfWinner = randomWords[0] % s_players.length;
+        address payable winner = s_players[indexOfWinner];
+
+        s_recentWinner = winner;
+        s_raffleState = RaffleState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        delete s_players;
+
+        emit WinnerPicked(winner);
+
+        (bool success, ) = winner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+    }
+
+    /** Getter Functions */
+
+    function getNumWords() public pure returns (uint256) {
+        return NUM_WORDS;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+
+    function getLastTimeStamp() public view returns (uint256) {
+        return s_lastTimeStamp;
+    }
+
+    function getInterval() public view returns (uint256) {
+        return i_interval;
+    }
+
+    function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
+    }
+
+    function getNumberOfPlayers() public view returns (uint256) {
+        return s_players.length;
     }
 }
